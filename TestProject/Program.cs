e89f000440962892ab1079692d653af3d004905a
Saki Basken <sbasken@tnsi.com>
2024-11-08 08:18:12 -0800

    Complete Conversions

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,39 +1,18 @@
-﻿// Console.WriteLine("Signed integral types:");
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
-// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+string strings = "";
+decimal decimals = 0.0m;
+foreach (var value in values)
+{
+    decimal parsedValue;
+    if (decimal.TryParse(value, out parsedValue))
+    {
+        decimals += parsedValue;
+    } else {
+        strings += value;
+    }
+}
 
-// Console.WriteLine("");
-// Console.WriteLine("Unsigned integral types:");
+Console.WriteLine($"Message: {strings}");
+Console.WriteLine($"Total: {decimals}");
 
-// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
-
-// Console.WriteLine("");
-// Console.WriteLine("Floating point types:");
-// Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-// Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-// Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
-
-int[] data = new int[3];
-
-int val_A = 2;
-int val_B = val_A;
-val_B = 5;
-
-Console.WriteLine("--Value Types--");
-Console.WriteLine($"val_A: {val_A}");
-Console.WriteLine($"val_B: {val_B}");
-
-int[] ref_A= new int[1];
-ref_A[0] = 2;
-int[] ref_B = ref_A;
-ref_B[0] = 5;
-
-Console.WriteLine("--Reference Types--");
-Console.WriteLine($"ref_A[0]: {ref_A[0]}");
-Console.WriteLine($"ref_B[0]: {ref_B[0]}");
