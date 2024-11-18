8ccd9b10d0ed6b8cb51b139006344bd0b6c915a0
Saki Basken <sbasken@tnsi.com>
2024-11-18 07:45:11 -0800

    Complete operations on arrays using helper methods in c#

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,18 +1,63 @@
-﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+﻿// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+// Console.WriteLine("");
 
-string strings = "";
-decimal decimals = 0.0m;
-foreach (var value in values)
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
+
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items = orderStream.Split(',');
+Array.Sort(items);
+
+foreach (var item in items)
 {
-    decimal parsedValue;
-    if (decimal.TryParse(value, out parsedValue))
+    if (item.Length == 4)
     {
-        decimals += parsedValue;
-    } else {
-        strings += value;
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine(item + "\t- Error");
     }
 }
 
-Console.WriteLine($"Message: {strings}");
-Console.WriteLine($"Total: {decimals}");
+
+
 
